6ef39df617fee11d60cfe1e4544d31cdac1debee
Fabian665 <fabian665@gmail.com>
2022-08-25 12:09:57 +0300

    Added graphs.py and example graph

---
+++ graphs.py
@@ -0,0 +1,27 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+dod = {
+    "A": {
+        "E": {},
+        "D": {},
+        "C": {}
+    },
+    "B": {
+        "D": {}
+    },
+    "C": {
+        "A": {},
+        "B": {}
+    },
+    "D": {},
+    "E": {
+        "A": {},
+        "D": {}
+    }
+}
+
+G = nx.from_dict_of_dicts(dod, create_using=nx.DiGraph)
+print([p for p in nx.all_shortest_paths(G, "C", "D")])
+nx.draw(G, with_labels=True)
+plt.show()
