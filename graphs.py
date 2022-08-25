d04c7d44f8f7c17704f1f98c6f9a3a0734815099
Fabian665 <fabian665@gmail.com>
2022-08-25 12:15:17 +0300

    Changed from dict of dicts to dict of lists

--- graphs.py
+++ graphs.py
@@ -2,26 +2,14 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 dod = {
-    "A": {
-        "E": {},
-        "D": {},
-        "C": {}
-    },
-    "B": {
-        "D": {}
-    },
-    "C": {
-        "A": {},
-        "B": {}
-    },
-    "D": {},
-    "E": {
-        "A": {},
-        "D": {}
-    }
+    "A": ["E", "D", "C"],
+    "B": ["D"],
+    "C": ["A", "B"],
+    "D": [],
+    "E": ["A", "D"]
 }
 
-G = nx.from_dict_of_dicts(dod, create_using=nx.DiGraph)
+G = nx.from_dict_of_lists(dod, create_using=nx.DiGraph)
 print([p for p in nx.all_shortest_paths(G, "C", "D")])
 nx.draw(G, with_labels=True)
 plt.show()
