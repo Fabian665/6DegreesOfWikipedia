8f46573ca9328371b608f9059fc787efe03d4d4c
Fabian665 <fabian665@gmail.com>
2022-08-31 13:16:44 +0300

    Added read_from_index_file, get_neighbours, get_id_from_name and create_json_file

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
+def read_from_index_file():
+    """generator for id  of pages from the index file"""
+    pass
+
+
+def get_neighbours(page_id):
+    """het neighbours of given page_id"""
+    pass
+
+
+def get_id_from_name(page_id):
+    pass
+
+
+def create_json_file():
+    pass
+
+
 def main():
     graph = nx.complete_graph(5)
     nx.draw(graph)
--- wiki.py
+++ wiki.py
@@ -2,5 +2,5 @@ import wikipedia
 
 
 ny = wikipedia.search("New York City", results=1, suggestion=False)
-print(ny[0])
-print(wikipedia.WikipediaPage(ny[0]).url)
+print(dir(ny[0]))
+print(wikipedia.WikipediaPage(pageid=1874).links)
